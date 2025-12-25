3cf438942cfc6d0e2d4268da424f177de5423395
jsakatsume <j.sakatsume@gmail.com>
2025-12-25 18:53:57 +0900

    style: ruff format

--- src/ds-hdp-hmm/permute.py
+++ src/ds-hdp-hmm/permute.py
@@ -4,14 +4,15 @@
 import numpy as np
 from munkres import Munkres
 
+
 def compute_cost(zt, zt_real):
-    cost_mat = [] #np.zeros((len(np.unique(zt_real)), len(np.unique(zt))));
+    cost_mat = []  # np.zeros((len(np.unique(zt_real)), len(np.unique(zt))));
     K_use = max(len(np.unique(zt_real)), len(np.unique(zt)))
     for ii in range(K_use):  ## real
         cost_mat.append([])
         for jj in range(K_use):
-            cost_mat[ii].append((np.abs((zt_real==ii)*1 - (zt==jj)*1)).sum())
-    #print_matrix(cost_mat);
+            cost_mat[ii].append((np.abs((zt_real == ii) * 1 - (zt == jj) * 1)).sum())
+    # print_matrix(cost_mat);
 
     m = Munkres()
     indexes = m.compute(cost_mat)
@@ -20,6 +21,6 @@ def compute_cost(zt, zt_real):
     for row, column in indexes:
         value = cost_mat[row][column]
         total += value
-        #print(f'({row}, {column}) -> {value}')
-    #print(f'total cost: {total}')
+        # print(f'({row}, {column}) -> {value}')
+    # print(f'total cost: {total}')
     return total, indexes
